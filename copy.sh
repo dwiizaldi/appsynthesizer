bfa734ba175826022beae45853c201e6a13453d2
dwiizaldi <dwi.izaldi@gmail.com>
2018-12-07 16:58:10 +0800

    new roles for destroying all

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 cp descriptorfile.yml ./roles/preparenet/vars/main.yml
 cp descriptorfile.yml ./roles/clonecomponents/vars/main.yml
 cp descriptorfile.yml ./roles/bootvm/vars/main.yml
+cp descriptorfile.yml ./roles/destroy/vars/main.yml
